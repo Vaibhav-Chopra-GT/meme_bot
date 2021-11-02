78c929b39a581526bbdb2685df11871d3d99d495
Vaibhav <nightfurydgreat@gmail.com>
2021-11-02 20:18:40 +0530

    Adding files for a bot

---
+++ twiiterbot.py
@@ -0,0 +1,53 @@
+import tweepy
+import time
+import os
+
+CONSUMER_KEY = os.environ.get('c_key')
+CONSUMER_SECRET = os.environ.get('c_secret')
+ACCESS_KEY = os.environ.get('acc_key')
+ACCESS_SECRET = os.environ.get("acc_secret")
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+filename = "lastid.txt"
+
+
+def getlastid(file):
+    fread = open(file, "r")
+    lastid = int(fread.read().strip())
+    fread.close()
+    return lastid
+
+
+def putid(id, file):
+    fwrite = open(file, "w")
+    fwrite.write(str(id))
+    fwrite.close()
+    return
+
+
+def reply():
+    lstseenid = getlastid(filename)
+    mentions = api.mentions_timeline(
+        since_id=lstseenid,
+    )
+    for mention in reversed(mentions):
+        print(str(mention.id) + "  " + mention.text)
+
+        lstseenid = mention.id
+        putid(lstseenid, filename)
+        media = api.media_upload(r"C:\Users\Vaibhav Chopra\Downloads\imagesfennec.jpg")
+
+        post_result = api.update_status(
+            "@" + mention.user.screen_name,
+            in_reply_to_status_id=mention.id,
+            media_ids=[media.media_id],
+        )
+
+
+while True:
+    reply()
+    time.sleep(15)
