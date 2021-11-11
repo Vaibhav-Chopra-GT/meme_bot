fc8ec6929b6ab629a01eb781c6d6a8c37dd72ebb
Vaibhav <nightfurydgreat@gmail.com>
2021-11-11 21:59:47 +0530

    Replaced usage of heroku api

--- meme_bot/download_image.py
+++ meme_bot/download_image.py
@@ -1,12 +1,21 @@
 import requests
+import random
+
+
+x = random.randrange(2, 27, 1)
 
 
 def downloadmeme():
-    r = requests.get("https://meme-api.herokuapp.com/gimme/memes")
+
     # json is used for web dictionary
-    link = r.json()["url"]
+    r = requests.get(
+        "https://www.reddit.com/r/memes.json", headers={"User-agent": "your bot 0.1"}
+    )
+
+    link = r.json()["data"]["children"][x]["data"]["url"]
 
     p = requests.get(link)
     with open("meme.jpg", "wb") as f:
         f.write(p.content)
+
     return
